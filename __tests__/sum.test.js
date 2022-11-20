3a3b6d3628867afc70b39e268fe130f8a3a6b8b5
Yahoo <yac033@ucsd.edu>
2022-11-19 21:20:43 -0800

    Fix Sum test

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -3,3 +3,7 @@ const sum = require('../code-to-unit-test/sum.js');
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1,2)).toBe(3);
 });
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(1 + 2).toBe(3);
+});
