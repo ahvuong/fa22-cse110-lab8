8d80827967fea1168ae427d49e36985c8acbcb62
ahvuong <110503579+ahvuong@users.noreply.github.com>
2022-11-19 20:50:37 -0800

    Update Step 5 and 8 in lab8.test.js

--- __tests__/lab8.test.js
+++ __tests__/lab8.test.js
@@ -126,7 +126,7 @@ describe('Basic user flow for Website', () => {
     // TODO - Step 5
     // At this point the item 'cart' in localStorage should be 
     // '[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]', check to make sure it is
-    const data = await page.$$eval('product-item', () => {
+    const data = await page.evaluate(() => {
       return window.localStorage.getItem('cart');
     });
 
@@ -192,7 +192,7 @@ describe('Basic user flow for Website', () => {
     console.log('Checking the localStorage...');
     // TODO - Step 8
     // At this point he item 'cart' in localStorage should be '[]', check to make sure it is
-    const data = await page.$$eval('product-item', () => {
+    const data = await page.evaluate(() => {
       return window.localStorage.getItem('cart');
     });
 
